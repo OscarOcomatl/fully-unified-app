bea84261722c41647d494cda19298852acee7ed6
OscarDev13 <oscarperezocomatldev@gmail.com>
2024-08-23 00:16:59 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/fully_unified_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fully_unified_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fully unified app'),
+        ),
+        body: const Center(
+          child: Text('Fully unified app'),
+        ),
+      )
+    );
+  }
+}
